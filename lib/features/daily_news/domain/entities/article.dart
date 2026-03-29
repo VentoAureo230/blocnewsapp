ec78d917ed553555c40297d7799e66fcb13552cb
VentoAureo230 <anthony.meny35@gmail.com>
2026-03-29 14:16:17 +0200

    Adding entity with equatable

---
+++ lib/features/daily_news/domain/entities/article.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable {
+  final int ? id;
+  final String ? author;
+  final String ? title;
+  final String ? description;
+  final String ? url;
+  final String ? urlToImage;
+  final String ? publishedAt;
+  ArticleEntity({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+      ];
+}
