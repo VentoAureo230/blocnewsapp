07b47993f414315c163a011eb58f8cc354429479
VentoAureo230 <anthony.meny35@gmail.com>
2026-03-29 14:24:44 +0200

    feat: repository class and model

---
+++ lib/features/daily_news/data/models/article.dart
@@ -0,0 +1,26 @@
+import 'package:blocnewsapp/features/daily_news/domain/entities/article.dart';
+
+class ArticleModel extends ArticleEntity {
+  const ArticleModel({
+    int? id,
+    String? author,
+    String? title,
+    String? description,
+    String? url,
+    String? urlToImage,
+    String? publishedAt,
+    String? content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> map) {
+    return ArticleModel(
+      author: map['author'] as String?,
+      title: map['title'] as String?,
+      description: map['description'] as String?,
+      url: map['url'] as String?,
+      urlToImage: map['urlToImage'] as String?,
+      publishedAt: map['publishedAt'] as String?,
+      content: map['content'] as String?,
+    );
+  }
+}
