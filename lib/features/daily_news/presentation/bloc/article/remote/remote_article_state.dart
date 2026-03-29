c3e7d4792189f4641c94118258cc3d9f6c619cac
VentoAureo230 <anthony.meny35@gmail.com>
2026-03-29 16:32:05 +0200

    Impl of the BLoC logic

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,26 @@
+import 'package:blocnewsapp/features/daily_news/domain/entities/article.dart';
+import 'package:equatable/equatable.dart';
+import 'package:dio/dio.dart';
+
+abstract class RemoteArticleState extends Equatable {
+  final List<ArticleEntity> ? articles;
+  final DioException ? error;
+
+  const RemoteArticleState({this.articles, this.error});
+
+  @override
+  List<Object?> get props => [articles!, error!];
+}
+
+class RemoteArticleLoading extends RemoteArticleState {
+  const RemoteArticleLoading() : super();
+}
+
+class RemoteArticleLoaded extends RemoteArticleState {
+  const RemoteArticleLoaded({required List<ArticleEntity> articles})
+      : super(articles: articles);
+}
+
+class RemoteArticleError extends RemoteArticleState {
+  const RemoteArticleError(DioException error) : super(error: error);
+}
