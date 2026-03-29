a016aaae3e4e8766cad8b45990a95f537dc9f0bc
VentoAureo230 <anthony.meny35@gmail.com>
2026-03-29 15:56:23 +0200

    doc

--- lib/features/daily_news/data/data_sources/remote/news_api_service.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -6,6 +6,8 @@ import 'package:blocnewsapp/features/daily_news/data/models/article.dart';
 
 part 'news_api_service.g.dart';
 
+/// This file is responsible for handling all the networks call methods
+/// Code is generated using the retrofit package
 @RestApi(baseUrl: "https://newsapi.org/v2")
 abstract class NewsApiService {
   factory NewsApiService(Dio dio) = _NewsApiService;
